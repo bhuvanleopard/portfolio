8d64e1195d01b9198c895765a6aa8f3f305df38e
bhuvanleopard <cbhuvan08@gmail.com>
2025-10-24 01:15:45 +0530

    .

--- src/components/FluidBackground.tsx
+++ src/components/FluidBackground.tsx
@@ -76,6 +76,40 @@ export const FluidBackground = () => {
               repeatType: "mirror",
             }}
           />
+
+          {/* Blob 2: Bottom Right (MODIFIED for more wobble) */}
+          <motion.div
+            className="
+              absolute left-[25%] top-[0%] w-[150vh] h-[150vh]  
+              bg-[radial-gradient(circle_at_center,transparent,transparent,transparent,theme(colors.purple.500))]
+              blur-[6px]"
+            animate={{
+              x: ["0%", "-5%"],
+              y: ["-15%", "-10%"],
+              rotate: [30, 60],
+              borderRadius: [
+                "60% 90% 70% 80% / 60% 90% 70% 80%", // Original
+                "85% 75% 65% 95% / 70% 80% 90% 60%",
+                "70% 100% 80% 90% / 90% 70% 80% 100%",
+                "95% 65% 75% 85% / 80% 90% 60% 70%",
+                "120% 80% 100% 100% / 120% 60% 100% 100%", // Original
+                "70% 90% 60% 80% / 100% 80% 70% 90%",
+                "80% 60% 90% 70% / 90% 70% 100% 60%",
+                "65% 85% 95% 75% / 60% 100% 80% 90%",
+                "55% 75% 80% 60% / 65% 70% 60% 65%", // Original
+                "90% 60% 70% 100% / 80% 90% 60% 70%",
+                "75% 95% 85% 65% / 70% 60% 90% 80%",
+                "60% 80% 65% 70% / 60% 65% 70% 75%", // Original
+              ],
+            }}
+            transition={{
+              duration: 32, // Increased duration to smooth out more keyframes
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "mirror",
+            }}
+          />
+
         </div>
       </motion.div>
     </div>
