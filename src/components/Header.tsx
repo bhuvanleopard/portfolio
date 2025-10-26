4ddd7b9aa9e9f2337be73876aff4dd87d0e600c2
bhuvanleopard <cbhuvan08@gmail.com>
2025-10-26 08:57:19 +0530

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,24 +29,6 @@ const Header = function(){
                     backdrop-blur-sm">
                     <ResumeBtn/>
                 </div>
-                {/* <a  className="text-2xl tracking-widest font-extrabold text-white bg-white/5 
-                    inline-flex items-center justify-between gap-6 cursor-pointer py-1 px-2 
-                    -skew-x-28 border border-white/8 backdrop-blur-sm rounded-sm"
-                    href={Resume}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={(e) => {
-                        window.open("https://drive.google.com/file/d/1z_ErdQcVY_uY1HOaYF4zVBAfq1683iTm/view?usp=sharing", "_blank");
-
-                        const link = document.createElement("a");
-                        link.href = Resume;
-                        link.download = "Bhuvan_Chavan_Resume.pdf";
-                        document.body.appendChild(link);
-                        link.click();
-                        document.body.removeChild(link);
-
-                        e.preventDefault();}}>RESUME
-                </a> */}
         </header>
     )
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,25 @@
-import { FiMenu } from "react-icons/fi";
+import { MdMenu, MdMenuOpen } from "react-icons/md";
 import ResumeBtn from "./ResumeBtn";
 const NavBar = function(){
 
     const toggleMobileMenu = ()=>{
         
         const mobileMenu = document.getElementById("mobileMenu");
+        const menuOpen = document.getElementById("menuOpen");
+        const menuClose = document.getElementById("menuClose");
         const mainSection = document.body;
 
         if(mobileMenu?.classList.contains('hidden')){
 
             mobileMenu.classList.remove("hidden");
+            menuOpen?.classList.remove("hidden");
+            menuClose?.classList.add("hidden")
             mainSection?.classList.add("overflow-hidden")
         }else{
 
             mobileMenu?.classList.add("hidden");
+            menuOpen?.classList.add("hidden");
+            menuClose?.classList.remove("hidden")
             mainSection?.classList.remove("overflow-hidden")
         }
     }
@@ -30,13 +36,18 @@ const NavBar = function(){
         </nav>
         
 
-        <button onClick= {toggleMobileMenu} className="relative flex text-4xl md:hidden z-100 cursor-pointer backdrop-blur-sm">
-            <FiMenu/>
+        <button onClick= {toggleMobileMenu} id="menuClose" className="relative flex text-4xl md:hidden z-100 cursor-pointer backdrop-blur-sm">
+            <MdMenu/>
+        </button>
+
+
+        <button onClick= {toggleMobileMenu} id="menuOpen" className="hidden pt-1.5 relative text-4xl md:hidden z-100 cursor-pointer backdrop-blur-sm">
+            <MdMenuOpen/>
         </button>
 
         
         <div id='mobileMenu' className="hidden fixed items-center pt-[8rem]  w-full h-full bottom-0 right-0 left-0 p-4 md:hidden
-         bg-black/90 z-90">
+         bg-black/90 z-90 ">
             <nav className="flex flex-col gap-12 items-center px-4">
             <a onClick={toggleMobileMenu} className="cursor-pointer text-2xl tracking-[8px] transition-colors hover:text-gray-300 z-100 font-extrabold px-6 py-3 border-white/50 text-center" href="#about-me">HOME</a>
             <a onClick={toggleMobileMenu} className="cursor-pointer text-2xl tracking-[8px] transition-colors hover:text-gray-300 z-100 font-extrabold px-6 py-3 border-white/50 text-center" href="#skills">SKILLS</a>
